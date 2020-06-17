bf882106031dbbc92c9cd340910dea3f65d786da
Henrik <hgg1998@gmail.com>
2020-06-17 08:52:15 +0200

    Add 0 pole

--- dra.m
+++ dra.m
@@ -1,28 +1,29 @@
 clearvars;
 
-G_numerator = [1 20 80];
-G_denominator = [1 2 8];
+% G_numerator = [1 20 80];
+% G_denominator = [1 2 8];
 
 sampling_freq = 256;
 T = 1 / sampling_freq;
 min_T_len = 6.5;
 num_samples = 2 ^ (ceil(log2(min_T_len * sampling_freq)));
-hd = finely_sampled_continuous_time_impuls_response(G_numerator, G_denominator, sampling_freq, T, num_samples);
-
+hd = impuls_response(sampling_freq, T, num_samples);
+% hd = finely_sampled_continuous_time_impuls_response(G_numerator, G_denominator, sampling_freq, T, num_samples);
 h_step = continuous_time_step_response(hd, T);
 
 td = T * (0 : num_samples - 1);
 T_shifted = 0.1;
 [time_vector, h_pulse] = unit_pulse_response(h_step, T_shifted, min_T_len, td);
-[A_est, B_est, C_est, D_est] = ho_kalman(h_pulse, 2)
+[A_est, B_est, C_est, D_est] = ho_kalman(h_pulse, 4, T_shifted, 0);
 
 % Plot
-stem(time_vector, h_pulse,'filled'); 
+stem(time_vector, h_pulse,'filled');
 hold on;
-H_true = tf(G_numerator, G_denomenator);
+H_true = tf([-0.5 -3], [1 6 2]);
 [himpDiscTrue, timpDiscTrue] = impulse(c2d(H_true, T_shifted), 5);
 himpDiscTrue = T_shifted * himpDiscTrue;
-plot(timpDiscTrue, himpDiscTrue, 'r.', 'markersize',8);
+plot(timpDiscTrue, himpDiscTrue, 'r.', 'markersize', 8);
+grid on;
 axis([-0.01 5 -0.8 2.3]);
 
 function [time_vector, h_pulse] = unit_pulse_response(h_step, T_shifted, min_T_len, td)
@@ -32,6 +33,13 @@ end
 
 function [h_step] = continuous_time_step_response(hd, T)
     h_step = T * cumsum(hd); 
+end
+
+function [hd] = impuls_response(sampling_freq, T, num_samples)
+    freq_vector = 0 : num_samples - 1;
+    s = (2j / T) * tan(pi * freq_vector / num_samples);
+    Hd = (-0.5 * s - 3) ./ (s .^ 2 + 6 * s + 2);
+    hd = real(ifft(Hd)) * sampling_freq;
 end
 
 function [hd] = finely_sampled_continuous_time_impuls_response(G_numerator, G_denominator, sampling_freq, T, num_samples)
@@ -53,7 +61,7 @@ function [hd] = finely_sampled_continuous_time_impuls_response(G_numerator, G_de
     hd = real(ifft(Hd)) * sampling_freq; 
 end
 
-function [A_estimate, B_estimate, C_estimate, D_estimate] = ho_kalman(unit_pulse, hankel_size)
+function [A_estimate, B_estimate, C_estimate, D_estimate] = ho_kalman(unit_pulse, hankel_size, T_shifted, res0)
     hankel_matrix = hankel(unit_pulse(2:end));
     H = hankel_matrix(1:hankel_size, 1:hankel_size);
     H_shifted = hankel_matrix(2:hankel_size + 1, 1:hankel_size);
@@ -68,8 +76,8 @@ function [A_estimate, B_estimate, C_estimate, D_estimate] = ho_kalman(unit_pulse
     extended_observability = U * sigma;
     extended_controllability = sigma * V';
 
-    A_estimate = pinv(extended_observability) * H_shifted * pinv(extended_controllability);
-    B_estimate = extended_controllability(:, 1);
-    C_estimate = extended_observability(1, :);
-    D_estimate = unit_pulse(1);
+    A_estimate = [pinv(extended_observability) * H_shifted * pinv(extended_controllability) zeros(system_order, 1); 1 zeros(1, system_order)];
+    B_estimate = [extended_controllability(:, 1); T_shifted]; 
+    C_estimate = [extended_observability(1, :), res0];
+    D_estimate = [unit_pulse(1)];
 end
