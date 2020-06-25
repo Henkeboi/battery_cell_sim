6a2135644f217da8500083b84a06997d20067a64
Henrik <hgg1998@gmail.com>
2020-06-25 17:20:29 +0200

    Fix sign in calculate_ocv_pos

--- calculate_ocv_pos.m
+++ calculate_ocv_pos.m
@@ -1,3 +1,4 @@
-cse_pos = 8000;  % TODO: Calcultate solid_concentration_pos
-phi = cse_pos / solid_max_concentration_pos;
-ocv_pos = 4.192829 - 0.0565661 * tanh(-14.5546 * phi + 8.60942) - 0.0275479 * (1 / ((0.998432 -phi) ^ 0.4924656) - 1.90111) - 0.157123 * exp(-0.04738 * (phi ^ 6)) + 0.810239 * exp(-40 * (phi - 0.133875));
+function [ocv_pos] = calculate_ocv_pos(cse_pos, const)
+    phi = cse_pos / const.solid_max_c_pos;
+    ocv_pos = 4.19829 + 0.0565661 * tanh(-14.5546 * phi + 8.60942) - 0.0275479 * ((1 / ((0.998432 - phi) ^ 0.4924656)) - 1.90111) - 0.157123 * exp(-0.04738 * (phi ^ 6)) + 0.810239 * exp(-40 * (phi - 0.133875));
+end
