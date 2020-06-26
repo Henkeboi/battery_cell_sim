4ac0717c7487076f1172158453bd13f5607738cb
Henrik <hgg1998@gmail.com>
2020-06-26 12:14:17 +0200

    Add cse tf with wrong tf0 and pole at 0

--- main.m
+++ main.m
@@ -3,111 +3,127 @@ run('parameters.m');
 
 cse_pos = 10000;
 cse_neg = 10000;
+
 z_coordinates = [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0];
-
-[all_tf_j_pos, res0, D, sampling_freq, T_len] = tf_j(cse_pos, z_coordinates, const, 'pos');
-A_estimates = [];
-B_estimates = [];
-C_estimates = [];
-D_estimates = [];
-for i = 1 : size(all_tf_j_pos, 2)
-    [A_est, B_est, C_est, D_est] = dra(all_tf_j_pos(:, i), res0, D, sampling_freq, T_len, const);
-    A_estimates = [A_estimates; A_est];
-    B_estimates = [B_estimates; B_est];
-    C_estimates = [C_estimates; C_est];
-    D_estimates = [D_estimates, D_est];
-end
+[tf_cse, res0, D, sampling_freq, T_len] = tf_cse(cse_neg, z_coordinates, const, 'neg');
+[A_est, B_est, C_est, D_est] = dra(tf_cse, res0, D, sampling_freq, T_len, const);
+sys = ss(A_est, B_est, C_est, D_est, 0.1);
 
 
-steps = 10000;
-li_flux_pos = zeros(steps, size(all_tf_j_pos, 2));
-X = zeros(size(A_estimates, 2), size(all_tf_j_pos, 2));
-for step = 1 : steps
-    for i = 1 : size(all_tf_j_pos, 2)
-        A = A_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1 : size(A_estimates, 2));
-        B = B_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1);
-        C = C_estimates(i, 1 : size(A_estimates, 2));
-        D = D_estimates(1, i);
-        X(:, i) = A * X(:, i) + B;
-        Y = C * X(:, i) + D;
-        li_flux_pos(step, i) = Y;
-    end
-end
-
-t = 1 : steps;
+% z_coordinates = [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0];
+% 
+% [all_tf_j_pos, res0, D, sampling_freq, T_len] = tf_j(cse_pos, z_coordinates, const, 'pos');
+% A_estimates = [];
+% B_estimates = [];
+% C_estimates = [];
+% D_estimates = [];
+% for i = 1 : size(all_tf_j_pos, 2)
+%     [A_est, B_est, C_est, D_est] = dra(all_tf_j_pos(:, i), res0, D, sampling_freq, T_len, const);
+%     A_estimates = [A_estimates; A_est];
+%     B_estimates = [B_estimates; B_est];
+%     C_estimates = [C_estimates; C_est];
+%     D_estimates = [D_estimates, D_est];
+% end
+% 
+% 
+% steps = 10000;
+% li_flux_pos = zeros(steps, size(all_tf_j_pos, 2));
+% X = zeros(size(A_estimates, 2), size(all_tf_j_pos, 2));
+% for step = 1 : steps
+%     for i = 1 : size(all_tf_j_pos, 2)
+%         A = A_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1 : size(A_estimates, 2));
+%         B = B_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1);
+%         C = C_estimates(i, 1 : size(A_estimates, 2));
+%         D = D_estimates(1, i);
+%         X(:, i) = A * X(:, i) + B;
+%         Y = C * X(:, i) + D;
+%         li_flux_pos(step, i) = Y;
+%     end
+% end
+% 
+% t = 1 : steps;
 % subplot(2, 1, 1);
-for i = 1 : size(li_flux_pos, 2)
-    % plot(t, li_flux_pos(:, i))
-    % text(max(t), max(li_flux_pos(:, i)), "Pos" + num2str(z_coordinates(i)));
-    % hold on;
-end
-grid on;
-
-[all_tf_j_neg, res0, D, sampling_freq, T_len] = tf_j(cse_neg, z_coordinates, const, 'neg');
-A_estimates = [];
-B_estimates = [];
-C_estimates = [];
-D_estimates = [];
-for i = 1 : size(all_tf_j_neg, 2)
-    [A_est, B_est, C_est, D_est] = dra(all_tf_j_neg(:, i), res0, D, sampling_freq, T_len, const);
-    A_estimates = [A_estimates; A_est];
-    B_estimates = [B_estimates; B_est];
-    C_estimates = [C_estimates; C_est];
-    D_estimates = [D_estimates, D_est];
-end
-
-
-steps = 10000;
-li_flux_neg = zeros(steps, size(all_tf_j_neg, 2));
-X = zeros(size(A_estimates, 2), size(all_tf_j_neg, 2));
-for step = 1 : steps
-    for i = 1 : size(all_tf_j_neg, 2)
-        A = A_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1 : size(A_estimates, 2));
-        B = B_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1);
-        C = C_estimates(i, 1 : size(A_estimates, 2));
-        D = D_estimates(1, i);
-        X(:, i) = A * X(:, i) + B;
-        Y = C * X(:, i) + D;
-        li_flux_neg(step, i) = Y;
-    end
-end
-
-t = 1 : steps;
+% for i = 1 : size(li_flux_pos, 2)
+%     plot(t, li_flux_pos(:, i))
+%     text(max(t), max(li_flux_pos(:, i)), "Pos" + num2str(z_coordinates(i)));
+%     hold on;
+% end
+% grid on;
+% 
+% [all_tf_j_neg, res0, D, sampling_freq, T_len] = tf_j(cse_neg, z_coordinates, const, 'neg');
+% A_estimates = [];
+% B_estimates = [];
+% C_estimates = [];
+% D_estimates = [];
+% for i = 1 : size(all_tf_j_neg, 2)
+%     [A_est, B_est, C_est, D_est] = dra(all_tf_j_neg(:, i), res0, D, sampling_freq, T_len, const);
+%     A_estimates = [A_estimates; A_est];
+%     B_estimates = [B_estimates; B_est];
+%     C_estimates = [C_estimates; C_est];
+%     D_estimates = [D_estimates, D_est];
+% end
+% 
+% steps = 10000;
+% li_flux_neg = zeros(steps, size(all_tf_j_neg, 2));
+% X = zeros(size(A_estimates, 2), size(all_tf_j_neg, 2));
+% for step = 1 : steps
+%     for i = 1 : size(all_tf_j_neg, 2)
+%         A = A_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1 : size(A_estimates, 2));
+%         B = B_estimates(i * size(A_estimates, 2) - size(A_estimates, 2) + 1: i * size(A_estimates, 2), 1);
+%         C = C_estimates(i, 1 : size(A_estimates, 2));
+%         D = D_estimates(1, i);
+%         X(:, i) = A * X(:, i) + B;
+%         Y = C * X(:, i) + D;
+%         li_flux_neg(step, i) = Y;
+%     end
+% end
+% 
+% t = 1 : steps;
 % subplot(2, 1, 2);
-for i = 1 : size(li_flux_neg, 2)
-    % plot(t, li_flux_neg(:, i))
-    % text(max(t), max(li_flux_neg(:, i)), "Neg" + num2str(z_coordinates(i)));
-    % hold on;
-end
+% for i = 1 : size(li_flux_neg, 2)
+%     plot(t, li_flux_neg(:, i))
+%     text(max(t), max(li_flux_neg(:, i)), "Neg" + num2str(z_coordinates(i)));
+%     hold on;
+% end
 % grid on;
 
-end_time = 22000;
-cse_neg =  25000;
-cse_pos =  21000;
-SOC = zeros(1, end_time);
-ocv_d_neg = zeros(1, end_time);
-ocv_d_pos = zeros(1, end_time);
-for i = 1 : end_time
-    SOC(1, i) = cse_neg / const.solid_max_c_neg;
-    ocv_d_neg(i) = calculate_ocv_derivative_neg(cse_neg, const);
-    ocv_d_pos(i) = calculate_ocv_derivative_pos(cse_pos, const);
-    cse_neg = cse_neg - 1;
-    cse_pos = cse_pos - 1;
-end
-t = 1 : end_time;
-subplot(3, 1, 1);
-plot(t, ocv_d_neg);
-text(max(t), min(ocv_d_pos), "Neg Uocv derivative");
-hold on;
-grid on;
-subplot(3, 1, 2);
-plot(t, ocv_d_pos);
-hold on;
-grid on;
-text(max(t), min(ocv_d_pos), "Pos Uocv derivative");
-subplot(3, 1, 3);
-plot(t, SOC)
-text(max(t), min(SOC), "SOC");
+
+
+
+
+
+
+% end_time = 22860 - 5500;
+% cse_neg =  26390;
+% cse_pos =  0 + 5000;
+% SOC = zeros(1, end_time);
+% ocv_d_neg = zeros(1, end_time);
+% ocv_d_pos = zeros(1, end_time);
+% for i = 1 : end_time
+%     SOC(1, i) = cse_neg / const.solid_max_c_neg;
+%     ocv_d_neg(i) = calculate_ocv_derivative_neg(cse_neg, const);
+%     ocv_d_pos(i) = calculate_ocv_derivative_pos(cse_pos, const);
+%     cse_neg = cse_neg - 1;
+%     cse_pos = cse_pos + 1;
+% end
+% t = 1 : end_time;
+% subplot(3, 1, 1);
+% plot(t, ocv_d_neg);
+% text(max(t), max(ocv_d_neg), "Neg Uocv");
+% hold on;
+% grid on;
+% subplot(3, 1, 2);
+% plot(t, ocv_d_pos);
+% hold on;
+% grid on;
+% text(max(t), min(ocv_d_pos), "Pos Uocv");
+% subplot(3, 1, 3);
+% plot(t, SOC)
+% text(max(t), min(SOC), "SOC");
+
+
+
+
 
 
 
@@ -151,9 +167,6 @@ text(max(t), min(SOC), "SOC");
 % grid on;
 
 
-
-
-fprintf("\n\n");
 
 
 
---
+++ tf_cse.m
@@ -0,0 +1,66 @@
+function [tf_cse, res0, D, sampling_f, T_len] = phi_se_tf(cse, z_coordinates, const, electrode)
+    sampling_f = 200;
+    T = 1 / sampling_f;
+    T_len = 1;
+    num_samples = 2 ^ (ceil(log2(sampling_f * T_len)));
+    f_vector = 0 : num_samples - 1;
+    s = zeros(1, size(f_vector, 2));
+    for i = 1 : size(f_vector, 2)
+        s(1, i) = 2j * sampling_f * tan(pi * f_vector(i) / num_samples);
+    end
+
+    % Declare params
+    if electrode == 'neg'
+        Rct = const.R_ct_neg;
+        Rfilm = const.R_film_neg;
+        Rse = Rct + Rfilm;
+        Rs = const.radius_neg;
+        L = const.L_neg;
+        F = const.F;
+        D = const.diffusivity_neg;
+        A = const.A_neg;
+        alpha = const.alpha_neg;
+        sigma = const.sigma_neg;
+        kappa = const.kappa_neg;
+        beta = Rs * sqrt(s / D);
+        eps = const.porosity_solid_neg;
+        Uovc_d = calculate_ocv_derivative_neg(cse, const);
+    elseif electrode == 'pos'
+        Rct = const.R_ct_neg;
+        Rfilm = const.R_film_neg;
+        Rse = Rct + Rfilm;
+        Rs = const.radius_neg;
+        L = const.L_neg;
+        F = const.F;
+        D = const.diffusivity_neg;
+        A = const.A_neg;
+        alpha = const.alpha_neg;
+        sigma = const.sigma_neg;
+        kappa = const.kappa_neg;
+        beta = Rs * sqrt(s / D);
+        eps = const.porosity_solid_neg;
+        Uovc_d = calculate_ocv_derivative_pos(cse, const);
+    else
+        error("Bad electrode selection");
+    end
+    
+    % Calculate nu
+    nu = L * sqrt(alpha / sigma + alpha / kappa) ./ sqrt(Rse + Uovc_d / F / D * (1 ./ (Rs * beta .* coth(beta))));
+    
+    % Calcualte lithium concentration on the electrode surface TF.
+    z = 0.5; % Temporary choosen z-location
+    tf_cse = (sigma * cosh(nu * z) + kappa * cosh(nu * (z - 1))) .* (Rs * nu) ./ (alpha * F * L * A * D * (kappa + sigma) * sinh(nu) .* (1 - sqrt(beta) .* coth(beta)));
+    tf_cse0 = 0;
+    res0 = 0;
+    D = 0;
+    
+    for i = 1 : size(s, 2)
+        if isnan(tf_cse(1, i)) && s(1, i) == 0
+            tf_cse(1, i) = tf_cse0;
+        end
+    end
+
+    if any(isnan(tf_cse))
+        error("NAN In tf_cse")
+    end
+end
