41b25a1d64ebf244bc26332a5e4d253063854a69
Henrik <hgg1998@gmail.com>
2020-06-26 13:35:19 +0200

    Wrap ocv_neg in a function

--- calculate_ocv_neg.m
+++ calculate_ocv_neg.m
@@ -1,5 +1,4 @@
-function [ocv_neg] = calculate_ocv_neg()
-    cse_neg = 4000; % TODO: calculate cse_neg
-    phi = cse_neg / solid_max_concentration_neg;
+function [ocv_neg] = calculate_ocv_neg(cse_neg, const)
+    phi = cse_neg / const.solid_max_c_neg;
     ocv_neg = -0.16 + 1.32 * exp(-3.0 * phi) + 10.0 * exp(-2000.0 * phi);
 end
