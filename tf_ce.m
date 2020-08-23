8fd1c909e381fdccbcb56ec3d88e589fc4f68816
Henrik <hgg1998@gmail.com>
2020-08-23 17:37:52 +0200

    Plot phi.

--- tf_ce.m
+++ tf_ce.m
@@ -35,35 +35,53 @@ function [] = tf_ce(cse, x_coordinates, T_len, sampling_f, const)
     k6 = S_56.k6;
     
     fun_eps = @(x) eps_e_n .* ((0 <= x) & (x < L_n)) + eps_e_s .* ((L_n <= x) & (x < L_n + L_s)) + eps_e_p .* ((L_n + L_s <= x) & (x <= L_tot));
-    fun_phi_n = @(x, lambda, k1) k1 .* cos(sqrt(lambda * fun_eps(x) / De_n) .* x) .* ((0 <= x) & (x < L_n));
-    fun_phi_s = @(x, lambda, k3, k4) ((k3 .* cos(sqrt(lambda * fun_eps(x) / De_s) .* x) + k4 .* sin(sqrt(lambda * fun_eps(x) / De_s) .* x))) .* ((L_n <= x) & (x < L_n + L_s)); 
-    fun_phi_p = @(x, lambda, k5, k6) ((k5 .* cos(sqrt(lambda * fun_eps(x) / De_p) .* x) + k6 .* sin(sqrt(lambda * fun_eps(x) / De_p) .* x)) .* ((L_n + L_s <= x) & (x <= L_tot)));
+    fun_phi_n = @(x, lambda, k1) k1 .* cos(sqrt(lambda .* fun_eps(x) ./ De_n) .* x) .* ((0 <= x) & (x < L_n));
+    fun_phi_s = @(x, lambda, k3, k4) ((k3 .* cos(sqrt(lambda .* fun_eps(x) ./ De_s) .* x) + k4 .* sin(sqrt(lambda .* fun_eps(x) ./ De_s) .* x))) .* ((L_n <= x) & (x < L_n + L_s)); 
+    fun_phi_p = @(x, lambda, k5, k6) ((k5 .* cos(sqrt(lambda .* fun_eps(x) ./ De_p) .* x) + k6 .* sin(sqrt(lambda .* fun_eps(x) ./ De_p) .* x)) .* ((L_n + L_s <= x) & (x <= L_tot)));
 
-    step_size = 10e-7;
+    step_size = 10e-6;
     disp("Calculating PHI neg.")
     xn_vector = 0 : step_size : L_n;
-    PHI_n = @(x, lambda, k1) trapz(fun_phi_n(xn_vector, lambda, k1) .^ 2 .* fun_eps(xn_vector) .* (0 <= x & x <= L_n));
+    PHI_n = @(x, lambda, k1) trapz(xn_vector, fun_phi_n(xn_vector, lambda, k1) .^ 2 .* fun_eps(xn_vector) .* (0 <= x & x <= L_n));
     disp("Calculating PHI sep.")
     xs_vector = L_n : step_size : L_n + L_s;
-    PHI_s = @(x, lambda, k3, k4) trapz(fun_phi_s(xs_vector, lambda, k3, k4) .^ 2 .* fun_eps(xs_vector) .* (L_n <= x & x <= L_n + L_s));
+    PHI_s = @(x, lambda, k3, k4) trapz(xs_vector, fun_phi_s(xs_vector, lambda, k3, k4) .^ 2 .* fun_eps(xs_vector) .* (L_n <= x & x <= L_n + L_s));
     disp("Calculate PHI pos.")
     xp_vector = L_n + L_s : step_size : L_tot;
-    PHI_p = @(x, lambda, k5, k6) trapz(fun_phi_p(xp_vector, lambda, k5, k6) .^ 2 .* fun_eps(xp_vector) .* (L_n + L_s <= x <= L_tot)); 
+    PHI_p = @(x, lambda, k5, k6) trapz(xp_vector, fun_phi_p(xp_vector, lambda, k5, k6) .^ 2 .* fun_eps(xp_vector) .* (L_n + L_s <= x <= L_tot)); 
     eq5 = PHI_n(xn_vector, lambda, k1) + PHI_s(xs_vector, lambda, k3, k4) + PHI_p(xp_vector, lambda, k5, k6) == 1;
     eq5 = isolate(eq5, k1);
-
 
     k3 = subs(k3, k1, rhs(eq5));
     k4 = subs(k4, k1, rhs(eq5));
     k5 = subs(k5, k1, rhs(eq5));
     k6 = subs(k6, k1, rhs(eq5));
 
-    l = 1;
-    k3 = eval(subs(k3, lambda, l));
-    k4 = eval(subs(k4, lambda, l));
-    k5 = eval(subs(k5, lambda, l));
-    k6 = eval(subs(k6, lambda, l));
-    k1 = eval(subs(rhs(eq5), lambda, l));
+    disp("Calculate lambda and constant pairs.");
+    lambda_list = [];
+    k1_list = [];
+    k3_list = [];
+    k4_list = [];
+    k5_list = []; 
+    k6_list = []; 
 
-    %eval(subs(PHI_n(xn_vector, lambda, k1) + PHI_s(xs_vector, lambda, k3, k4) + PHI_p(xp_vector, lambda, k5, k6), lambda, l))
+    for l = 1 : 10
+        disp("Lambda: " + l);
+        lambda_list = [lambda_list l];
+        k1_list = [k1_list eval(subs(rhs(eq5), lambda, l))];
+        k3_list = [k3_list eval(subs(k3, lambda, l))];
+        k4_list = [k4_list eval(subs(k4, lambda, l))];
+        k5_list = [k5_list eval(subs(k5, lambda, l))];
+        k6_list = [k6_list eval(subs(k6, lambda, l))];
+    end
+
+    phi = @(x) fun_phi_n(x, lambda_list, k1_list) + fun_phi_s(x, lambda_list, k3_list, k4_list) + fun_phi_p(x, lambda_list, k5_list, k6_list);
+    step_size = 10e-10;
+    x_vector = 0 : step_size : L_tot;
+    y_vector = [];
+    for i = 1 : size(x_vector, 2)
+        y_vector = [y_vector sum(phi(x_vector(i)))];
+    end
+
+    plot(x_vector, y_vector);
 end
